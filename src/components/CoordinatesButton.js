fef07acbb614286000e3bcb914f2882bdf62098f
Haloren <mckennakyler@gmail.com>
2021-02-24 23:21:31 -0700

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,23 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+function CoordinatesButton(props) {
+    // console.log("Coordinates", props)
+    
+    function handleClick(event) {
+        // console.log(event)
+        // event.preventDefault(); 
+
+        let coordinates = [event.clientX, event.clientY]
+        props.onReceiveCoordinates(coordinates)
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Coordinates Button
+        </button>
+    )
+}
+
+export default CoordinatesButton
+// WHY DOES THIS NOT PASS THE TESTS WHEN "event.preventDefault();" IS USED ?
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,27 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+function DelayedButton(props) {
+    // console.log("DelayButton", props)
+
+    function handleClick(event) {
+        // console.log(event)
+
+        // event.preventDefault();
+        // console.log(props.delay)
+        setTimeout(() => {
+            event.persist()
+            props.onDelayedClick(event)
+        }, 
+        props.delay)
+        
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Delayed Button
+        </button>
+    )
+}
+
+export default DelayedButton
